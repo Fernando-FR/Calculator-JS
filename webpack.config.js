88a1d989cb8c661de0ecbb9f12d3e908e2fbc87d
Fernando Franco <fernandofrancoprofissinal@gmail.com>
2021-10-03 13:21:28 -0300

    SassIntalled

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {  entry: './src/js/index.js',
     mode:'production',
     output: {
         filename: 'script.js',
         path: __dirname,
     },
+    plugins: [new MiniCssExtractPlugin({
+        filename: 'styles.css'
+    })],
+    module: {
+        rules: [
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+            MiniCssExtractPlugin.loader,
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+            ],
+          },
+        ],
+      },
 };
